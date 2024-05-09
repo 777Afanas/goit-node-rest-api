7c7fb57f3337f20609f5ac100acccc47e6c55682
777Afanas <serhii2111@yahoo.com>
2024-05-09 18:25:48 +0300

    start

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
+import "dotenv/config";
+
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 
 import contactsRouter from "./routes/contactsRouter.js";
+import "./db.js";
 
 const app = express();
 
 app.use(morgan("tiny"));
-app.use(cors());
-app.use(express.json());
+app.use(cors());  
 
 app.use("/api/contacts", contactsRouter);
 
-app.use((_, res) => {
+
+app.use((req, res, next) => {
   res.status(404).json({ message: "Route not found" });
 });
 
--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,13 +1,15 @@
+import Contact from "../models/contact.js";  
 
-import contactsServices from "../services/contactsServices.js";
 import {
   createContactSchema,
   updateContactSchema,
+  updateStatusContactSchema, 
 } from "../schemas/contactsSchemas.js";
+
 
 export const getAllContacts = async (req, res, next) => {
   try {
-    const result = await contactsServices.listContacts();
+    const result = await Contact.find();
 
     res.status(200).json(result);
   } catch (error) {
@@ -16,11 +18,13 @@ export const getAllContacts = async (req, res, next) => {
 };
 
 export const getOneContact = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await contactsServices.getContactById(id);
-    if (!result) {
-      res.status(404).json({ message: "Not found" });
+  const { id } = req.params;
+  
+  try {     
+    const result = await Contact.findById(id);
+
+    if (result === null) {
+      return res.status(404).json({ message: "Not found" });
     }
 
     res.status(200).json(result);
@@ -30,11 +34,12 @@ export const getOneContact = async (req, res, next) => {
 };
 
 export const deleteContact = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await contactsServices.removeContact(id);
-    if (!result) {
-      res.status(404).json({ message: "Not found" });
+  const { id } = req.params;
+
+  try {     
+    const result = await Contact.findByIdAndDelete(id);
+    if (result === null) {
+      return res.status(404).json({ message: "Not found" });
     }
 
     res.status(200).json(result);
@@ -54,11 +59,11 @@ export const createContact = async (req, res, next) => {
     convert: false,
   });
   if (error) {
-    return res.status(400).json({ message: "Filds must be filled" });
+    return res.status(400).json({ message: "Fields must be filled" });
   }
 
   try {
-    const result = await contactsServices.addContact(req.body);
+    const result = await Contact.create(contact);
     res.status(201).json(result);
   } catch (error) {
     next(error);
@@ -66,26 +71,30 @@ export const createContact = async (req, res, next) => {
 };
 
 export const updateContact = async (req, res, next) => {
-  const { id } = req.params;
-  const { name, email, phone } = req.body;
+  const { id } = req.params;   
+
+   const contact = {
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+  };
   
   if (Object.keys(req.body).length === 0) {
     return res
       .status(400)
-      .json({ message: "Body must have at least one field" });
+      .json({ message: "Body must have at least one field" }); 
   }
- 
-  const { error } = updateContactSchema.validate({ name, email, phone }); 
+     
+  const { error } = updateContactSchema.validate(contact);
  
   if (error) {
-    return res.status(400).json({ message: "Filds must be filled" });
+    return res.status(400).json({ message: "Fields must be filled" });
   }
 
-  try {     
-    const result = await contactsServices.updateContact(id, name, email, phone);
-
-    if (!result) {
-      res.status(404).json({ message: "Not found" });
+  try {         
+    const result = await Contact.findByIdAndUpdate(id, contact, { new: true});
+    if (result === null) {
+      return res.status(404).json({ message: "Not found" });
     }
 
     res.status(200).json(result);
@@ -94,4 +103,36 @@ export const updateContact = async (req, res, next) => {
   }
 };
 
+export const updateStatusContact = async (req, res, next) => {
+  const { id } = req.params;   
 
+   const contact = {
+    name: req.body.name,
+    email: req.body.email,
+     phone: req.body.phone,
+    favorite: req.body.favorite,
+  };
+  
+  if (Object.keys(req.body).length === 0) {
+    return res
+      .status(400)
+      .json({ message: "Body must have at least one field" }); 
+  }
+     
+  const { error } = updateStatusContactSchema.validate(contact);
+ 
+  if (error) {
+    return res.status(400).json({ message: "Fields must be filled" });
+  }
+
+  try {         
+    const result = await Contact.findByIdAndUpdate(id, contact, { new: true});
+    if (result === null) {
+      return res.status(404).json({ message: "Not found" });
+    }
+
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
