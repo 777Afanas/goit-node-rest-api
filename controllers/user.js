d1c8d4cca30d8f9e37f7b668be7e692ef15a07c1
Serhii Yatsenko <serhii2111@yahoo.com>
2024-05-19 22:45:23 +0300

    and

--- controllers/user.js
+++ controllers/user.js
@@ -1,32 +1,35 @@
 import * as fs from "node:fs/promises";
 import path from "node:path";
 import User from "../models/user.js";
+import Jimp from "jimp";
 
 async function uploadAvatar(req, res, next) {
-    try {
-      //   перемістили файл з папки TMP в папку public/avatars
-      await fs.rename(
-        req.file.path,
-        path.resolve("public/avatars", req.file.filename)
-      );
+  try {
+    const userAvatar = await Jimp.read(req.file.path);
+    await userAvatar.cover(250, 250).writeAsync(req.file.path);
 
-      // змінюємо значення поля аватар у користувача, додане при його
-      // створенні, ідентифікатор  req.user.id, поле Аватар { avatarURL: req.file.filename },
-      // повернемо нову версію користувача -  { new: true }
-      const user = await User.findByIdAndUpdate(
-        req.user.id,
-        { avatarURL: req.file.filename },
-        { new: true }
-        );
-        // як що не змогли оновити картинку - 
-        // дуже малоймовірно - не змогли користувача з таким ідентифікаторм
-        if (user === null) {
-            return res.status(404).send({ message: "User not fond" });                        
-        };          
-        // res.send(user);
-        res.status(200).json({ avatarURL: user.avatarURL});  
+    //   перемістили файл з папки TMP в папку public/avatars
+    await fs.rename(
+      req.file.path,
+      path.resolve("public/avatars", req.file.filename)
+    );
 
-    } catch (error) {
+    // змінюємо значення поля аватар у користувача, додане при його
+    // створенні, ідентифікатор  req.user.id, поле Аватар { avatarURL: req.file.filename },
+    // повернемо нову версію користувача -  { new: true }
+    const user = await User.findByIdAndUpdate(
+      req.user.id,
+      { avatarURL: req.file.filename },
+      { new: true }
+    );
+    // як що не змогли оновити картинку -
+    // дуже малоймовірно - не змогли користувача з таким ідентифікаторм
+    if (user === null) {
+      return res.status(404).send({ message: "User not fond" });
+    }
+    // res.send(user);
+    res.status(200).json({ avatarURL: user.avatarURL });
+  } catch (error) {
     next(error);
   }
 }
